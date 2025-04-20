9a4a0c91f5062f8a5a3ebfbb7efa14753ce27218
udupashyam <udupashyam@gmail.com>
2025-04-20 10:30:20 +0400

    Update app.py

--- app.py
+++ app.py
@@ -33,5 +33,6 @@ class FlaskAppTestCase(unittest.TestCase):
             response = fetch_info(PERSONAL_INFO_URL, '12345', 'Personal Information')
             self.assertIn('mock', response)
 
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(debug=True, host="0.0.0.0", port=port)
